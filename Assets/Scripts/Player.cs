faa8a5886a34a3d105bec2cf4babd52de9b004c4
Adam Farahat <farahatadam@gmail.com>
2025-01-25 12:55:23 -0500

    Complete Git respository

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /*Things player needs:
+    - Movement speed
+    - Crouch
+    - Prone
+    - Interact
+    - Drag and Drop
+    */
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
